1b236aab80a27e15e6695e018b53dac073f57812
Eslam1811477 <eslamelnemery477@gmail.com>
2026-01-31 15:40:24 +0200

    new start

--- src/extension.ts
+++ src/extension.ts
@@ -1,75 +1,41 @@
-import * as vscode from 'vscode';
-import * as fs from 'fs';
+// import {
+//   enableHotReload,
+//   hotRequireExportedFn,
+//   registerUpdateReconciler,
+// } from '@hediet/node-reload'
+// import { Disposable } from '@hediet/std/disposable'
+import * as vscode from 'vscode'
+// import MyWebview from './MyWebview'
+import { NextWebviewPanel } from './NextWebview'
+
+// if (process.env.NODE_ENV === 'development') {
+//   enableHotReload({ entryModule: module })
+// }
+// registerUpdateReconciler(module)
+
+// export class Extension {
+//   public readonly dispose = Disposable.fn()
+
+//   constructor() {
+//     super()
+
+//     // Disposables are disposed automatically on reload.
+//     const item = this.dispose.track(vscode.window.createStatusBarItem())
+//     item.text = 'Hallo Welt'
+//     item.show()
+//   }
+// }
 
 export function activate(context: vscode.ExtensionContext) {
-
-  const provider = new DevSessionViewProvider(context);
-
   context.subscriptions.push(
-    vscode.window.registerWebviewViewProvider(
-      'devsession.view',
-      provider
-    )
-  );
+    vscode.commands.registerCommand('devsession', () => {
+      const webview = NextWebviewPanel.getInstance({
+        extensionUri: context.extensionUri,
+        route: 'view1',
+        title: 'Devsession',
+        viewId: 'ghnextA',
+      })
+    }),
+  )
+
 }
-
-class DevSessionViewProvider implements vscode.WebviewViewProvider {
-
-  constructor(
-    private readonly context: vscode.ExtensionContext
-  ) {}
-
-  resolveWebviewView(webviewView: vscode.WebviewView) {
-
-    webviewView.webview.options = {
-      enableScripts: true,
-      localResourceRoots: [
-        vscode.Uri.joinPath(this.context.extensionUri, 'assets')
-      ]
-    };
-
-    const htmlPath = vscode.Uri.joinPath(
-      this.context.extensionUri,
-      'assets',
-      'index.html'
-    );
-
-    webviewView.webview.html = this.getHtml(
-      webviewView.webview,
-      htmlPath
-    );
-  }
-
-  private getHtml(
-    webview: vscode.Webview,
-    htmlPath: vscode.Uri
-  ): string {
-
-    let html = fs.readFileSync(htmlPath.fsPath, 'utf8');
-
-    const assetsUri = webview.asWebviewUri(
-      vscode.Uri.joinPath(this.context.extensionUri, 'assets')
-    );
-
-    html = html.replace(/(src|href)="(.+?)"/g, (_, attr, value) => {
-      return `${attr}="${assetsUri}/${value}"`;
-    });
-
-    html = html.replace(
-      /<head>/,
-      `<head>
-        <meta http-equiv="Content-Security-Policy"
-          content="
-            default-src 'none';
-            img-src ${webview.cspSource};
-            script-src ${webview.cspSource};
-            style-src ${webview.cspSource} 'unsafe-inline';
-          ">
-      `
-    );
-
-    return html;
-  }
-}
-
-export function deactivate() {}
